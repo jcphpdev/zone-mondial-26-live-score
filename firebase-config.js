1dab9f6069c234c073907fa173b989931bea6b42
CHATBI Jaouad <j.chatbi@mmsp.gov.ma>
2026-06-24 18:21:45 +0100

    first commit

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,7 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
+// Cette configuration Firebase Web est publique par conception.
+// La protection des écritures est assurée par Firebase Authentication
+// et les règles de Realtime Database.
+export const firebaseConfig = {
   apiKey: "AIzaSyDIjQ5lfPcahptcaihe099tIYOCJ9IUnFk",
   authDomain: "zone-mondial-26.firebaseapp.com",
   databaseURL: "https://zone-mondial-26-default-rtdb.europe-west1.firebasedatabase.app",
@@ -14,5 +11,6 @@ const firebaseConfig = {
   appId: "1:892698565295:web:d71a652f65cecbbbeb525a"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const firebaseConfigured = Object.values(firebaseConfig).every(
+  value => typeof value === "string" && value.length > 0
+);
